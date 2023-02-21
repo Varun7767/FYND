3d2eee69a4e0aff19561ba5a3d9b9a30480036fa
Varun7767 <rajeshirkevarun@gmail.com>
2023-02-21 20:36:04 +0530

    21-2-2023

---
+++ MEVN/NODEJS/basic_express.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express(); // 
+
+app.get("/", (req, res) => {
+    res.send("route at root");
+});
+
+app.get("/hello", (req, res) => {
+    res.send("route at hello");
+});
+
+app.listen(3000, () => {
+    console.log("Listening on 3000");
+});
+
